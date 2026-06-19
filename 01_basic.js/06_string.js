ea921547799d254a4723e1eb621268041b5ed16c
dishajais05 <jaiswaldisha25@gmail.com>
2026-06-19 12:38:05 +0000

    string

---
+++ 01_basic.js/06_string.js
@@ -0,0 +1,36 @@
+const name = "disha"
+const repoCount = 50 
+console.log(name + repoCount + " value");
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('disha-hc')
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+// const anotherString = gameName.slice(-7,4)
+// console.log(anotherString);
+
+// const newStringOne = "      disha     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://disha.com/disha%20jaiswal"
+console.log(url.replace('%20',  ' '));
+console.log(url.includes('disha'));
+console.log(url.includes('sundar'));
+ 
+const Name = "disha-hc-com"
+console.log(Name.split('-'));
+
+
+
+
+
+
