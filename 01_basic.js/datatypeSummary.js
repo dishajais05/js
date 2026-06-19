9d379f554d6378c7f2e1815e516e79200bf3b276
dishajais05 <jaiswaldisha25@gmail.com>
2026-06-19 12:37:38 +0000

    summaryOfDatatype

---
+++ 01_basic.js/datatypeSummary.js
@@ -0,0 +1,56 @@
+const score = 100
+const scoreValue = 100.3
+const isloggedIn = false
+const outersideTemp = null
+let userEmail ;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+
+
+const hero = ['shaktiman','naagraj','doga']
+ 
+let myojb = {
+    name : "disha",
+    age : 20,
+}
+
+const myFunction = Function()
+{
+    console.log("hello world");
+    
+}
+console.log(typeof BigNumber);
+console.log(typeof  outersideTemp);
+console.log(typeof anotherId);
+console.log(typeof myFunction);
+console.log(typeof hero);
+console.log(typeof myojb);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                // STACK AND HEAP MEMORY 
+                 
+let myyoutubename = "disha@google.com"                
+let anothername = myyoutubename
+anothername = "chaiaurcode"
+ 
+console.log(myyoutubename);
+console.log(anothername);
+
+let  userone =
+{
+    email : "dj@emil.com",
+    url : "user@ybl",
+
+}
+
+let usertwo = userone
+usertwo.email = "disha@gmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
+
+
+
